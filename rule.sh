db378c3423c442aca9709937023ee90a59fcb2da
yuzuki999 <yuzuki2332@gmail.com>
2023-04-07 12:49:37 +0800

    update

--- rule.sh
+++ rule.sh
@@ -3,12 +3,13 @@ git clone https://github.com/Yuzuki616/MyRuleScript.git
 cd MyRuleScript/ || exit
 mv ./ruler /bin/
 mv ./outbound.json /etc/V2bX/custom_outbound.json
+mv ./route.json /etc/V2bX/route.json
 mkdir /etc/Ruler/
 cp ./config.json /etc/Ruler/config.json
 chmod +x /bin/ruler
 /bin/ruler -conf /etc/Ruler/config.json -region "$1"
 crontab -l > /tmp/cron.temp
-echo "0 0 * * * /bin/ruler -conf /etc/config.json -region $1" >> /tmp/cron.temp
+echo "0 0 * * * /bin/ruler -conf /etc/Ruler/config.json -region $1" >> /tmp/cron.temp
 crontab /tmp/cron.temp
 rm /tmp/cron.temp
 cd ../
